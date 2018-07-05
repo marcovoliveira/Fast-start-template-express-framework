65a273c11880ab11e8d0e9178031ee176a98e585
Marco Oliveira <marcooliveirapias@gmail.com>
2018-07-05 12:46:17 +0100

    add more pages and routes

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+var app = require('express')();
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/two', (req, res) => res.render('two'))
+
+app.get('/three', (req, res) => res.render('three'))
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
